3dbafb8cd67cb6dc1f7a2801b86af1aadd77b674
Beytullah7 <66316440+Beytullah7@users.noreply.github.com>
2020-07-21 00:22:29 +0200

    Add files via upload

--- src/FensterHighscore.java
+++ src/FensterHighscore.java
@@ -15,16 +15,17 @@ import javax.swing.JButton;
 @SuppressWarnings("serial")
 public class FensterHighscore extends JFrame {
 
-	private JPanel contentPane;
+	JPanel contentPane;
 	JButton btnZurckZumMen;
-
+	JLabel lblMotivation;
 	public void setWindowDimension(int x1,int y1,int x2,int y2) {
 		setBounds(x1,y1,x2,y2);
 	}
 	/**
 	 * Create the frame.
+	 * @param anzahlRichtigeAntworten 
 	 */
-	public FensterHighscore() {
+	public FensterHighscore(int anzahlRichtigeAntworten) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,7 +68,7 @@ public class FensterHighscore extends JFrame {
 			lblDuHastBisher.setBounds(146, 81, 135, 56);
 			contentPane.add(lblDuHastBisher, gbc_lblDuHastBisher);
 				
-			JLabel lblAnzahl = new JLabel("XY W\u00F6rter");
+			JLabel lblAnzahl = new JLabel(anzahlRichtigeAntworten+ " W\u00F6rter richtig beantwortet");
 			GridBagConstraints gbc_lblAnzahl = new GridBagConstraints();
 			gbc_lblAnzahl.insets = new Insets (0, 0, 5, 5);
 			gbc_lblAnzahl.gridx = 1;
@@ -75,7 +76,18 @@ public class FensterHighscore extends JFrame {
 			lblAnzahl.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 18));
 			lblAnzahl.setBounds(156, 112, 102, 56);
 			contentPane.add(lblAnzahl, gbc_lblAnzahl);
-			
+
+
+			lblMotivation = new JLabel(Motivation());
+			GridBagConstraints gbc_lblMotivation = new GridBagConstraints();
+			gbc_lblMotivation.insets = new Insets (0, 0, 5, 5);
+			gbc_lblMotivation.gridx = 1;
+			gbc_lblMotivation.gridy = 4;
+			lblMotivation.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 18));
+			lblMotivation.setBounds(92, 158, 252, 25);
+			contentPane.add(lblMotivation,gbc_lblMotivation);
+
+			/**
 			JLabel lblKorrektBeantwortet = new JLabel("am St\u00FCck korrekt beantwortet !");
 			GridBagConstraints gbc_lblKorrekt = new GridBagConstraints();
 			gbc_lblKorrekt.insets = new Insets (0, 0, 5, 5);
@@ -93,7 +105,37 @@ public class FensterHighscore extends JFrame {
 			lblMaximalKannst.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 11));
 			lblMaximalKannst.setBounds(60, 236, 350, 14);
 			contentPane.add(lblMaximalKannst, gbc_lblMaxi);
-			
+			*/
 		
 	}
+	public String Motivation(){
+		
+		
+		double Prozent=(1/10);
+		
+		
+		if (Prozent == 0){
+			return ("Du kannst nur besser werden!");
+		}
+		if(Prozent <= 0.1) {
+			return ("Das wird langsam aber sicher, nur nicht aufgeben!");
+		}
+		if(Prozent <= 0.3) {
+			return ("Es ist noch kein Meister vom Himmel gefallen. Immer flei§ig am Ball bleiben");
+		}
+		if(Prozent <= 0.5) {
+			return ("Sehr gut immer weiter machen!");
+		}
+		if(Prozent <= 0.7) {
+			return ("Prima du wirst immer besser!");
+		}
+		if(Prozent <= 0.98) {
+			return ("Das kann sich sehen lassen! Mal gucken ob du es beim nŠchsten mal auch schaffst!");
+		}
+		else  {
+			return ("Alles Richtig gemacht , NŠchstes mal bringst du mir was bei :D");
+		}
+	}
+
+	
 }
