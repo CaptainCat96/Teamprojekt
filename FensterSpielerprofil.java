7f47a676e177d241394372054e512fc430485d15
HenrikHuether <65134760+HenrikHuether@users.noreply.github.com>
2020-07-23 13:43:46 +0200

    Add files via upload

--- FensterSpielerprofil.java
+++ FensterSpielerprofil.java
@@ -4,20 +4,39 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.ImageIcon;
 import java.awt.Toolkit;
 import javax.swing.JButton;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollBar;
 import java.awt.Image;
-
+import java.awt.Insets;
+import java.util.List;
+import java.util.*;
 
 @SuppressWarnings("serial")
 public class FensterSpielerprofil extends JFrame {
 
-	private JPanel contentPane;
+	JPanel contentPane;
 	JButton buttonback;
 	JLabel lblNewLabel;
+	
+	
+
+//NEU###################################
+	Set<String> vokEng = new HashSet<String>();
+	int eng = 0 + vokEng.size();
+	Set<String> vokEsp = new HashSet<String>();
+	int esp = 0 + vokEsp.size();
+	Set<String> vokTurk = new HashSet<String>();
+	int turk = 0+vokTurk.size();
+	
+	
+//NEU-END#############################	
+	
 
 	public void setWindowDimension(int x1,int y1,int x2,int y2) {
 		setBounds(x1,y1,x2,y2);
@@ -26,81 +45,160 @@ public class FensterSpielerprofil extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	JLabel lblRang;
 	public FensterSpielerprofil() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\levin\\Downloads\\icons8-erinnerung-48.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 680, 465);
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0,0.0,0.0, };
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0,0.0, };
+		contentPane.setLayout(gbl_contentPane);
+
 		buttonback = new JButton("<--");
-		buttonback.setBounds(47, 326, 89, 23);
-		contentPane.add(buttonback);
+		GridBagConstraints gbc_buttonBack = new GridBagConstraints();
+		gbc_buttonBack.gridx = 0;
+		gbc_buttonBack.gridy = 0;
+		contentPane.add(buttonback,gbc_buttonBack);	
 		
 		JLabel lblUserprofile = new JLabel("Userprofile");
-		lblUserprofile.setBounds(236, 26, 98, 29);
+		GridBagConstraints gbc_Userprofile = new GridBagConstraints();
+		gbc_Userprofile.gridx = 1;
+		gbc_Userprofile.gridy = 0;
 		lblUserprofile.setFont(new Font("Yu Gothic UI", Font.BOLD, 15));
-		contentPane.add(lblUserprofile);
+		contentPane.add(lblUserprofile, gbc_Userprofile);
 		
-		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBounds(472, 138, 130, 38);
-		progressBar.setMaximum(50);
-		progressBar.setValue(10);
-		contentPane.add(progressBar);
-		
-		JLabel lblUsernameMaxMustermann = new JLabel("Username: Max Mustermann");
-		lblUsernameMaxMustermann.setBounds(35, 236, 147, 23);
-		lblUsernameMaxMustermann.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblUsernameMaxMustermann);
-		
-		JLabel lblAge = new JLabel("Age: 25");
-		lblAge.setBounds(35, 259, 79, 23);
-		lblAge.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblAge);
-		
-		JLabel lblCurrentStatus = new JLabel("Current Status:");
-		lblCurrentStatus.setBounds(408, 96, 73, 15);
-		lblCurrentStatus.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblCurrentStatus);
-		
-		JLabel lblEnglish = new JLabel("English (10/50)");
-		lblEnglish.setBounds(319, 138, 89, 38);
-		lblEnglish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblEnglish);
-		
-		JLabel lblSpanish = new JLabel("Spanish (40/50)");
-		lblSpanish.setBounds(319, 208, 89, 38);
-		lblSpanish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblSpanish);
-		
-		JProgressBar progressBar_1 = new JProgressBar();
-		progressBar_1.setBounds(472, 208, 130, 38);
-		progressBar_1.setValue(40);
-		progressBar_1.setMaximum(50);
-		contentPane.add(progressBar_1);
-		
-		JLabel lblTurkish = new JLabel("Turkish (20/50)");
-		lblTurkish.setBounds(319, 280, 132, 38);
-		lblTurkish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		contentPane.add(lblTurkish);
-		
-		JProgressBar progressBar_2 = new JProgressBar();
-		progressBar_2.setBounds(472, 279, 130, 38);
-		progressBar_2.setValue(20);
-		progressBar_2.setMaximum(50);
-		contentPane.add(progressBar_2);
-		
-		lblNewLabel = new JLabel("");
 		try {
+			lblNewLabel = new JLabel("");
 			Image image = new ImageIcon(this.getClass().getResource("Icon.png")).getImage();
+			GridBagConstraints gbc_image = new GridBagConstraints();
+			gbc_image.gridx= 1;
+			gbc_image.gridy= 1;
 			lblNewLabel.setIcon(new ImageIcon(image));
-			lblNewLabel.setBounds(34, 71, 203, 157);
-			contentPane.add(lblNewLabel);
+			contentPane.add(lblNewLabel,gbc_image);
 		}catch(Exception e) {
-			System.out.println("Profilbild konnte nicht gefunden werden!");
+			System.out.println("Profilbild nicht vorhanden");
 		}
 		
+	
+		
+		JLabel lblUsernameMaxMustermann = new JLabel("Username: Max Mustermann");
+		GridBagConstraints gbc_UserName = new GridBagConstraints();
+		gbc_UserName.gridx = 1;
+		gbc_UserName.gridy = 2;
+		lblUsernameMaxMustermann.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblUsernameMaxMustermann, gbc_UserName);
+		
+		JLabel lblAge = new JLabel("Age: 25");
+		GridBagConstraints gbc_Age = new GridBagConstraints();
+		gbc_Age.gridx = 1;
+		gbc_Age.gridy = 3;
+		gbc_Age.anchor = GridBagConstraints.WEST;
+		lblAge.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblAge, gbc_Age);
+ 
+		
+		JLabel lblRang0 = new JLabel("Rang:");
+		GridBagConstraints gbc_Rang0= new GridBagConstraints();
+		gbc_Rang0.gridx = 3;
+		gbc_Rang0.gridy = 1;
+		lblRang0.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblRang0, gbc_Rang0);	
+		
+		lblRang = new JLabel(Rang());
+		GridBagConstraints gbc_lblRang = new GridBagConstraints();
+		gbc_lblRang.gridx = 4;
+		gbc_lblRang.gridy = 1;
+		gbc_lblRang.anchor = GridBagConstraints.WEST;
+		lblAge.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblRang, gbc_lblRang);
+
+		JLabel lblCurrentStatus = new JLabel("Current Status:");
+		GridBagConstraints gbc_Current= new GridBagConstraints();
+		gbc_Current.gridx = 3;
+		gbc_Current.gridy = 2;
+		lblCurrentStatus.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblCurrentStatus, gbc_Current);		
+		
+//NEU#########################################################################
+		
+		JLabel lblEnglish = new JLabel("English (" + eng + "/50)");
+		GridBagConstraints gbc_Englisch= new GridBagConstraints();
+		gbc_Englisch.gridx = 3;
+		gbc_Englisch.gridy = 3;
+	
+		gbc_Englisch.fill = GridBagConstraints.BOTH; 
+		gbc_Englisch.insets = new Insets( 5, 5, 5, 5);
+		lblEnglish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblEnglish, gbc_Englisch);
+		
+		JProgressBar progressBar = new JProgressBar();
+		GridBagConstraints gbc_progressBar = new GridBagConstraints();
+		gbc_progressBar.gridx = 4;
+		gbc_progressBar.gridy = 3;
+		progressBar.setMaximum(50);
+		progressBar.setValue(eng);
+		contentPane.add(progressBar, gbc_progressBar);
+		
+		JLabel lblSpanish = new JLabel("Spanish (" + esp + "/50)");
+		GridBagConstraints gbc_Spanisch= new GridBagConstraints();
+		gbc_Spanisch.gridx = 3;
+		gbc_Spanisch.gridy = 4;
+		gbc_Spanisch.insets = new Insets( 5, 5, 5, 5);
+		lblSpanish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblSpanish,gbc_Spanisch);
+		
+		JProgressBar progressBar_1 = new JProgressBar();
+		GridBagConstraints gbc_ProgressBar_1= new GridBagConstraints();
+		gbc_ProgressBar_1.gridx = 4;
+		gbc_ProgressBar_1.gridy = 4;
+		progressBar_1.setValue(esp);
+		progressBar_1.setMaximum(50);
+		contentPane.add(progressBar_1, gbc_ProgressBar_1);
+		
+		JLabel lblTurkish = new JLabel("Turkish (" + turk + "/50)");
+		GridBagConstraints gbc_Turkish= new GridBagConstraints();
+		gbc_Turkish.gridx = 3;
+		gbc_Turkish.gridy = 5;
+		gbc_Turkish.insets = new Insets( 5, 5, 5, 5);
+		lblTurkish.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		contentPane.add(lblTurkish, gbc_Turkish);
+		
+		JProgressBar progressBar_2 = new JProgressBar();
+		GridBagConstraints gbc_ProgressBar_2= new GridBagConstraints();
+		gbc_ProgressBar_2.gridx = 4;
+		gbc_ProgressBar_2.gridy = 5;
+		progressBar_2.setValue(turk);
+		progressBar_2.setMaximum(50);
+		contentPane.add(progressBar_2, gbc_ProgressBar_2);
+		
+		
+		
+		
 	}
+	
+	public String Rang(){
+		int Gesamtpunkte = 70;
+		
+		if (Gesamtpunkte <= 50){
+			return ("Amateur");
+		}
+		if(Gesamtpunkte <= 100) {
+			return ("Amateur 2.0");
+		}
+		if (Gesamtpunkte <=200) {
+			return ("Profi Amateur");
+		}
+		if (Gesamtpunkte <=300) {
+			return ("Fortgeschrittener");
+		}
+		else {return ("Meister der Sprachen");}
+		}
+	
 }
--- Run.java
+++ Run.java
@@ -1,8 +1,16 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Main Class
@@ -12,6 +20,7 @@ import java.awt.event.ActionListener;
  */
 @SuppressWarnings("serial")
 public class Run extends JFrame implements ActionListener {
+
 	/*
 	 * #############################################################################
 	 * #############################################################################
@@ -23,23 +32,24 @@ public class Run extends JFrame implements ActionListener {
 	int WindowSizeY1 = 0;
 	int WindowSizeX2 = 800;
 	int WindowSizeY2 = 800;
+
 	/**
 	 * Farbschema: Default,Schwarz, Blau, Rot
 	 */
 	public void ColorDesign() {
-		String Ausgewaehlt="Default";
-		Color Schwarz= new Color(0,0,0);
-		String Blau= "#1736E6";
-		String Rot= "#E30302";
+		String Ausgewaehlt = "Default";
+		Color Schwarz = new Color(0, 0, 0);
+		String Blau = "#1736E6";
+		String Rot = "#E30302";
 	}
-	
-	
-	
+
 	public Run() {
-		
-		starte_FensterStartmenu();
+		// playAudio();
+		starte_FensterLogin();
+		// starte_FensterStartmenue();
+		this.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
 	}
-	
+
 	public void setWindowDimension(int x1, int y1, int x2, int y2) {
 		setBounds(x1, y1, x2, y2);
 	}
@@ -56,44 +66,90 @@ public class Run extends JFrame implements ActionListener {
 	 * ###### Action Listener
 	 */
 	@Override
+
 	public void actionPerformed(ActionEvent e) {
-		// FensterStarmen�
-		if (e.getSource() == Startmenu.btnZumSpiel) {
-			ResetSpiel();
-			starte_FensterSpiel();
-			Startmenu.dispose();
+
+		// Fenster login
+		if (e.getSource() == login.btnLogin) {
+			if (!login.isUserInputValid()) {
+				JOptionPane.showMessageDialog(null, "You have to enter a name and password!", "Login error",
+						JOptionPane.ERROR_MESSAGE);
+			} else {
+				if (login.isAccountValid()) {
+					login.dispose();
+					String user = login.getUser();
+					starte_FensterStartmenue();
+				} else {
+					JOptionPane.showMessageDialog(null, "Incorrect account", "Login", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		}
-		if (e.getSource() == Startmenu.btnDeinProfil) {
+
+		if (e.getSource() == login.btnRegister) {
+			if (!login.isUserInputValid()) {
+				JOptionPane.showMessageDialog(null, "You have to enter a name and password!", "Login error",
+						JOptionPane.ERROR_MESSAGE);
+			} else {
+				if (login.registerUser()) {
+					login.dispose();
+					String user = login.getUser();
+					starte_FensterStartmenue();
+				} else {
+					JOptionPane.showMessageDialog(null, "Cannot register user", "Register", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+
+		}
+		// FensterStarmenue
+		if (e.getSource() == Startmenue.rdbtnTonAus) {
+
+			if (Startmenue.rdbtnTonAus.isSelected() == false) {
+				System.out.println("Resume playing Audio");
+				playAudio();
+			} else {
+				System.out.println("Stop Playing Audio");
+				this.stopAudio();
+			}
+		}
+		if (e.getSource() == Startmenue.btnZumSpiel) {
+			starte_FensterSpiel(Startmenue.holeAusgewaehltesSpiel());
+		}
+		if (e.getSource() == Startmenue.btnDeinProfil) {
 			starte_Spielerprofil();
 		}
-		if (e.getSource() == Startmenu.btnEinstellungen) {
+		if (e.getSource() == Startmenue.btnEinstellungen) {
 			starte_FensterEinstellungen();
 		}
 		// FensterSpiel
-		if (e.getSource() == Spiel.btnNext) {
-			GameWindow_nextQuestion();
+		try {// falls Fenster nicht Konstuiert ist
+			if (e.getSource() == Spiel.btnNext) {
+				GameWindow_nextQuestion();
+			}
+			if (e.getSource() == Spiel.buttonBack) {
+				System.out.println("beende spiel");
+				allePanelsSchliessen();
+				starte_FensterStartmenue();
+			}
+		} catch (Exception e4) {
+			// Fenster noch nicht konsturiert
 		}
-		if (e.getSource() == Spiel.buttonBack) {
-			Highscore.dispose();
-			Spiel.dispose();
-			starte_FensterStartmenu();
-		}
+
 		// FensterSpielerprofil
 		if (e.getSource() == Spielerprofil.buttonback) {
-			Startmenu.dispose();
-			starte_FensterStartmenu();
-			;
+			allePanelsSchliessen();
+			starte_FensterStartmenue();
 		}
 		// FensterHighscore
 		if (e.getSource() == Highscore.btnZurckZumMen) {
-			Highscore.dispose();
-			Spiel.dispose();
-			starte_FensterStartmenu();
+			allePanelsSchliessen();
+			starte_FensterStartmenue();
 		}
-		//FensterEinstellungen
+
+		// FensterEinstellungen
 		if (e.getSource() == Einstellungen.buttonBack) {
-			Einstellungen.dispose();
-			starte_FensterStartmenu();
+			allePanelsSchliessen();
+			starte_FensterStartmenue();
+
 		}
 	}
 
@@ -102,74 +158,167 @@ public class Run extends JFrame implements ActionListener {
 	 * #############################################################################
 	 * ###### Fenster Logik
 	 */
-	FensterStartmenu Startmenu = new FensterStartmenu();
+	public void allePanelsSchliessen() {
+		try {
+			this.remove(login.contentPane);
+			this.remove(Startmenue.contentPane);
+			this.remove(Einstellungen.contentPane);
+			this.remove(Spielerprofil.contentPane);
+			this.remove(Spiel.contentPane);
+			this.remove(Highscore.contentPane);
 
-	public void starte_FensterStartmenu() {
-		// Load Layout
-		Startmenu.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
-		Startmenu.setVisible(true);
-		// ActionListener
-		Startmenu.btnZumSpiel.addActionListener(this);
-		Startmenu.btnDeinProfil.addActionListener(this);
-		Startmenu.btnEinstellungen.addActionListener(this);
-		Startmenu.rdbtnTonAus.addActionListener(this);
+		} catch (Exception e) {
+			System.out.println("Warnung:Es sind noch nicht alle Fenster erstellt worden.");
+		}
+
+		this.repaint();
 	}
 
+	boolean ersteIterationFuerLogin = true;
+	FensterLogin login = new FensterLogin();
+
+	public void starte_FensterLogin() {
+		System.out.println("Create Login");
+		allePanelsSchliessen();
+		this.add(login.contentPane);
+		this.setVisible(true);
+		login.btnLogin.addActionListener(this);
+		login.btnRegister.addActionListener(this);
+		ersteIterationFuerLogin = false;
+	}
+
+	boolean ersteIterationFuerStartmenue = true;
+	FensterStartmenue Startmenue = new FensterStartmenue(daten);
+
+	public void starte_FensterStartmenue() {
+		System.out.println("Create Startmenue");
+		allePanelsSchliessen();
+		this.add(Startmenue.contentPane);
+		this.setVisible(true);
+		if (ersteIterationFuerStartmenue) {
+			// ActionListener
+			Startmenue.btnZumSpiel.addActionListener(this);
+			Startmenue.btnDeinProfil.addActionListener(this);
+			Startmenue.btnEinstellungen.addActionListener(this);
+			Startmenue.rdbtnTonAus.addActionListener(this);
+			ersteIterationFuerStartmenue = false;
+		}
+		setColor(Startmenue.contentPane);
+
+	}
+
+	private void setColor(JPanel panel) {
+		if (Einstellungen.color != null) {
+			panel.setBackground(Einstellungen.color);
+		}
+	}
+
+	boolean ersteIterationFuerSpiel = true;
 	FensterSpiel Spiel = new FensterSpiel();
 
-	public void starte_FensterSpiel() {
-		// Load Layout
-		Spiel.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
-		Spiel.setVisible(true);
-		// ActionListener
+	public void starte_FensterSpiel(String lernEinheit) {
+		System.out.println("Create Spiel");
+		allePanelsSchliessen();
+		this.add(Spiel.contentPane);
+		this.setVisible(true);
+
+		Spiel.btnNext.removeActionListener(this);
+		Spiel.buttonBack.removeActionListener(this);
+
 		Spiel.btnNext.addActionListener(this);
 		Spiel.buttonBack.addActionListener(this);
-		// Game Logik
-		
-		daten.loadTxtFile("src\\Lerneinheiten\\ExerciseBusinessEnglisch.txt");
-		GameWindow_nextQuestion();// erste Frage einblenden
+		/**
+		 * if(ersteIterationFuerSpiel) { // ActionListener
+		 * Spiel.btnNext.addActionListener(this);
+		 * Spiel.buttonBack.addActionListener(this); ersteIterationFuerSpiel=false; }
+		 */
+
+		String dateiname = daten.getFilenameForLerneinheit(lernEinheit);
+		if (dateiname != null) {
+			System.out.println("Lade Spieldaten: " + "src\\Lerneinheiten\\" + dateiname);
+			daten.loadTxtFile("src\\Lerneinheiten\\" + dateiname);
+			// daten.loadTxtFile("src/Lerneinheiten/" + dateiname);
+			GameWindow_nextQuestion();// erste Frage einblenden
+		} else {
+			System.err.println("Keine Datei für Lerneinheit " + lernEinheit);
+		}
+		setColor(Spiel.contentPane);
 	}
 
+	boolean ersteIterationFuerSpielerprofil = true;// Operationen wie action Listener nur einmal ausführen!
 	FensterSpielerprofil Spielerprofil = new FensterSpielerprofil();
 
 	public void starte_Spielerprofil() {
-		Spielerprofil.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
-		Spielerprofil.setVisible(true);
-		// ActionListener
-		Spielerprofil.buttonback.addActionListener(this);
+		System.out.println("Create Spielerprofil");
+		allePanelsSchliessen();
+		this.add(Spielerprofil.contentPane);
+		if (ersteIterationFuerSpielerprofil) {
+			Spielerprofil.buttonback.addActionListener(this);
+			ersteIterationFuerSpielerprofil = false;
+		}
+		this.setVisible(true);
+		setColor(Spielerprofil.contentPane);
 	}
 
-	FensterHighscore Highscore = new FensterHighscore();
+	boolean ersteIterationFuerHighscore = true;// Operationen wie action Listener nur einmal ausführen!
+	FensterHighscore Highscore;
 
 	public void starte_FensterHighscore() {
-		Highscore.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
-		Highscore.setVisible(true);
+		System.out.println("Create Highscore");
+		Highscore = new FensterHighscore(AnzahlRichtigeAntworten, maxAnzahlFragen);
+		allePanelsSchliessen();
+		this.add(Highscore.contentPane);
+		if (ersteIterationFuerHighscore) {
+			Highscore.btnZurckZumMen.addActionListener(this);
+			ersteIterationFuerHighscore = false;
+		}
+		this.setVisible(true);
 		// ActionListener
-		Highscore.btnZurckZumMen.addActionListener(this);
+		setColor(Highscore.contentPane);
+
 	}
-	
+
+	boolean ersteIterationFuerEinstellungen = true;// Operationen wie action Listener nur einmal ausführen!
 	FensterEinstellungen Einstellungen = new FensterEinstellungen();
+
 	public void starte_FensterEinstellungen() {
-		Einstellungen.setWindowDimension(WindowSizeX1, WindowSizeY1, WindowSizeX2, WindowSizeY2);
-		Einstellungen.setVisible(true);
+		System.out.println("Create Einstellungen");
+		allePanelsSchliessen();
+		this.add(Einstellungen.contentPane);
+
+		this.setVisible(true);
 		// ActionListener
-		Einstellungen.buttonBack.addActionListener(this);
+		if (ersteIterationFuerEinstellungen) {
+			Einstellungen.buttonBack.addActionListener(this);
+			ersteIterationFuerEinstellungen = false;
+		}
+
 	}
+
 	/**
 	 * #############################################################################
 	 * #############################################################################
 	 * ###### Spiel Logik
 	 */
-	//Variablen
+	// Variablen
 	int aktuelleFrageIndex = 0;
-	int maxAnzahlFragen = daten.maxAnzahl;
-	String userAntwort, Antwort;
+	int maxAnzahlFragen = daten.getMaxAnzahlFragen();
+	String userAntwort, Loesung;
 	// Variablen f�r Highscore statistiken
 	int AnzahlFalscheAntworten = 0;
 	int AnzahlRichtigeAntworten = 0;
+
 	public void GameWindow_nextQuestion() {
-		maxAnzahlFragen = daten.getFirstFreeIndex(daten.Fragen);
+		System.out.println("aktuelleFrageIndex " + aktuelleFrageIndex);
+		/**
+		 * Bugfix: Game lässt sich nicht neu starten
+		 */
+		if (maxAnzahlFragen == 0) {
+			System.out.println("Max anzahl fragen" + maxAnzahlFragen);
+			maxAnzahlFragen = daten.getFirstFreeIndex(daten.Fragen);
+		}
 		if (aktuelleFrageIndex >= maxAnzahlFragen) {
+			System.out.println("Spiel: zeige Highscore" + aktuelleFrageIndex + ">=" + maxAnzahlFragen);
 			starte_FensterHighscore();
 		}
 
@@ -187,52 +336,79 @@ public class Run extends JFrame implements ActionListener {
 			userAntwort = Spiel.btnPhone.getText();
 		}
 
-		// Bugfix: keine Antwort ausgew�hlt
+		// Bugfix: keine Antwort ausgewählt
 		if (Spiel.btnTree.isSelected() == false && Spiel.btnHouse.isSelected() == false
 				&& Spiel.btnStreet.isSelected() == false && Spiel.btnPhone.isSelected() == false) {
 			userAntwort = "keine Antowrt ausgew�hlt!";
 		}
-		// Bei Init �berspringen
+		// Bei Init überspringen
 		if (aktuelleFrageIndex != 0) {
-			Antwort = (String) daten.Antworten[aktuelleFrageIndex - 1];
-			System.out.println("Antwort ist: " + userAntwort);
-			System.out.println("Antwort ist :" + Antwort);
+			Loesung = daten.Antworten[aktuelleFrageIndex - 1];
+			// System.out.println("Antwort ist: " + userAntwort);
+			// System.out.println("Loesung ist :" + Loesung);
 			// Progress bar Aktualisieren
 			Spiel.progressBar.setMaximum(maxAnzahlFragen);
 			Spiel.progressBar.setValue(aktuelleFrageIndex);
 			// Falls Frage falsch beantwortet
-			if (userAntwort.equals(Antwort) == false) {
+			if (userAntwort.equals(Loesung) == false) {
 				AnzahlFalscheAntworten++;
-				Spiel.lblStatusLetzteAntwort.setText("Falsch. Richtige Antwort war: " + Antwort);
-				// lblStatusLetzteAntwort.setText("Falsch. Richtige Antwort war:"+Antwort+"(deine
+				Spiel.lblStatusLetzteAntwort.setText("Falsch. Richtige Antwort war: " + Loesung);
+				Spiel.lblStatusLetzteAntwort.setForeground(Color.RED);
+				// lblStatusLetzteAntwort.setText("Falsch. Richtige Antwort
+				// war:"+L�sung+"(deine
 				// Antwort: "+userAntwort+")");
 			}
 			// Falls Frage richtig beantwortet
-			if (userAntwort.equals(Antwort)) {
+			if (userAntwort.equals(Loesung)) {
 				AnzahlRichtigeAntworten++;
 				Spiel.lblStatusLetzteAntwort.setText("Richtig!");
+				Spiel.lblStatusLetzteAntwort.setForeground(Color.GREEN);
 			}
 		}
-		Spiel.btnTree.setText(daten.AntwortAlternativen[aktuelleFrageIndex][0]);
-		Spiel.btnHouse.setText(daten.AntwortAlternativen[aktuelleFrageIndex][1]);
-		Spiel.btnStreet.setText(daten.AntwortAlternativen[aktuelleFrageIndex][2]);
-		Spiel.btnPhone.setText(daten.AntwortAlternativen[aktuelleFrageIndex][3]);
-		Spiel.btnPencil.setVisible(false);
-		Spiel.btnRabbit.setVisible(false);
-		Spiel.txtWelchesWortIst.setText(daten.Fragen[aktuelleFrageIndex]);
-		Spiel.FragenVerbleibend.setText(aktuelleFrageIndex + " von " + maxAnzahlFragen);
-		aktuelleFrageIndex++;
-		System.out.println("N�chste Frage");
+		if (aktuelleFrageIndex < maxAnzahlFragen) {
+			Spiel.btnTree.setText(daten.AntwortAlternativen[aktuelleFrageIndex][0]);
+			Spiel.btnHouse.setText(daten.AntwortAlternativen[aktuelleFrageIndex][1]);
+			Spiel.btnStreet.setText(daten.AntwortAlternativen[aktuelleFrageIndex][2]);
+			Spiel.btnPhone.setText(daten.AntwortAlternativen[aktuelleFrageIndex][3]);
+			Spiel.btnPencil.setVisible(false);
+			Spiel.btnRabbit.setVisible(false);
+			Spiel.txtWelchesWortIst.setText(daten.Fragen[aktuelleFrageIndex]);
+			Spiel.FragenVerbleibend.setText(aktuelleFrageIndex + " von " + maxAnzahlFragen);
+			aktuelleFrageIndex++;
+			// System.out.println("Nächste Frage");
+
+		}
+
 	}
+
 	/**
 	 * #############################################################################
 	 * #############################################################################
 	 * ###### Hilfsmethoden
 	 */
-	public void ResetSpiel() {
-		System.out.println("ResetSpiel");
-		aktuelleFrageIndex = 0;
+
+	/*
+	 * Audio Player
+	 */
+
+	// File track1 = new File("enemy_missle.mp3");
+	Clip Audio;
+	AudioInputStream audioIn;
+
+	public void playAudio() {
+		try {
+			// Menu Music
+			audioIn = AudioSystem.getAudioInputStream(Run.class.getResource("TRACK1.wav"));
+			Audio = AudioSystem.getClip();
+			Audio.open(audioIn);
+			Audio.start();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-	
+
+	public void stopAudio() {
+		Audio.stop();
+	}
 
 }
